606645090736700804e6cc8aeb6d66b79428b6e1
yugdobariya1701 <138660299+yugdobariya1701@users.noreply.github.com>
2024-04-15 11:56:52 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -9,6 +9,25 @@ void printBoard(const vector<vector<char>>& board)
         cout << endl;
     }
 }
+bool gameOver(const vector<vector<char>>& board, char& winner)
+{
+    for (int i = 0; i < 3; ++i) 
+    {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '.') {
+            winner = board[i][0];
+            return true;
+        }
+    }
+ for (int j = 0; j < 3; ++j)
+     {
+        if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != '.')
+        {
+            winner = board[0][j];
+            return true;
+        }
+    }
+
+
 
 int main(){
 }
