876aab74afbf4442e4563c643a374ea0286a90fa
kenil059137 <148608360+kenil059137@users.noreply.github.com>
2024-04-15 11:55:13 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,4 +1,14 @@
 #include<iostream>
 using namespace std;
+void printBoard(const vector<vector<char>>& board)
+{
+    for (const auto& row : board) {
+        for (char cell : row) {
+            cout << cell << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
 }
