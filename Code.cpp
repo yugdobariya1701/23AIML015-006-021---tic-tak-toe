26de0e01f9f5aa823b7bf6adbb73df1316efb218
kenil059137 <148608360+kenil059137@users.noreply.github.com>
2024-04-15 11:58:47 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -26,6 +26,16 @@ bool gameOver(const vector<vector<char>>& board, char& winner)
             return true;
         }
     }
+ if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '.') {
+        winner = board[0][0];
+        return true;
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '.') {
+        winner = board[0][2];
+        return true;
+    }
+
+
 
 
 
